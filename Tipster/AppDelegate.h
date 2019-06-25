e3bbd1524d91b3a1cb202e7347e4b23e219ced96
Mario Baxter <mariobaxter@fb.com>
2019-06-25 14:01:55 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by mariobaxter on 6/25/19.
+//  Copyright © 2019 mariobaxter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
